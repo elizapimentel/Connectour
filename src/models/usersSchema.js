5cbb573ecc4c7154d9bed8ea8d9ebab6277cff31
elizapimentel <elizapimentel@hotmail.com>
2022-07-24 15:42:51 -0300

    :sparkles: feat: add users schema

---
+++ src/models/usersSchema.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    id: mongoose.Schema.Types.ObjectId,
+    name: {
+        type: String,
+        required: true
+    },
+    surname: {
+        type: String,
+        required: true,
+        unique: false
+    },
+    userName: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    county: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['admin', 'user'],
+        required: true
+    },
+    registrationNumber: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    createdAt: {
+        type: String,
+        default: new Date()
+    }
+}, {timestamps: true})
+
+module.exports = mongoose.model('user', userSchema);
