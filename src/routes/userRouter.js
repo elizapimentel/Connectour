b59e530144bc682f0ec21bb72f1692db23287695
elizapimentel <elizapimentel@hotmail.com>
2022-07-26 00:24:45 -0300

    :lock: add middlewares of verification

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const controller = require('../controllers/userController');
+const authController = require('../controllers/login');
+const { grantAccess } = require('../middleware/auth');
+const { allowIfLoggedin } = require('../middleware/auth')
 
-const controller = require('../controllers/userController')
-const authController = require('../controllers/login')
-const { checkAuth } = require('../middleware/auth')
-
-router.post("/create", controller.createUser)
-router.post("/login", authController.login)
-router.get("/all", controller.getAllUsers)
-router.get("/regnumber", controller.getByRegister)
-router.put("/update/:id", checkAuth, controller.updateUser)
-router.put("/updatePassword/:id", controller.updatePassword)
-router.delete("/delete/:id", checkAuth, controller.deleteUser)
+router.post("/signup", controller.signUp);
+router.post("/login", authController.login);
+router.get("/all", allowIfLoggedin, grantAccess('readAny', 'profile') ,controller.getAllUsers);
+router.get("/regnumber", allowIfLoggedin, grantAccess('readOwn', 'profile'), controller.getByRegister);
+router.put("/update/:id", allowIfLoggedin, grantAccess('updateAny', 'profile'), controller.updateUser);
+router.put("/updatePassword/:id", allowIfLoggedin, grantAccess('updateOwn', 'profile'), controller.updatePassword);
+router.delete("/delete/:id", allowIfLoggedin, grantAccess('deleteAny', 'profile'), controller.deleteUser);
 
 module.exports = router
