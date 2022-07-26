d1850370278edafa80a88f86570e3abae7851524
elizapimentel <elizapimentel@hotmail.com>
2022-07-26 00:23:54 -0300

    :lock: add middleware of verification

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 
 require('dotenv-safe').config();
 
+const app = express();
+
 const db = require('./config/mongoConfig');
+const usersSchema = require('./models/usersSchema');
 const userRoutes = require('./routes/userRouter');
 const postRoutes = require('./routes/postRouter');
 
@@ -12,7 +15,22 @@ db.connect();
 
 app.use(cors());
 app.use(express.json());
-app.use("/post", postRoutes);
+
+app.use(async (req, res, next) => {
+    if (req.headers["x-access-token"]) {
+     const token = req.headers["x-access-token"];
+     const { userId, exp } = await jwt.verify(token, process.env.SECRET);
+     // Check if token has expired
+     if (exp < Date.now().valueOf() / 1000) { 
+      return res.status(401).json({ error: "JWT token has expired, please login to obtain a new one" });
+     } 
+     res.locals.loggedInUser = await usersSchema.findById(userId); next(); 
+    } else { 
+     next(); 
+    } 
+   });
+
 app.use("/user", userRoutes);
+app.use("/post", postRoutes);
 
 module.exports = app;
