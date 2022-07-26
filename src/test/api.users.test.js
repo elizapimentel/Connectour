309c150d4044144b780deba2893f6c10cc79c56a
elizapimentel <elizapimentel@hotmail.com>
2022-07-26 20:58:43 -0300

    :white_check_mark: test: add test to create user

--- src/test/api.users.test.js
+++ src/test/api.users.test.js
@@ -1,3 +1,27 @@
 const request = require('supertest');
 const app = require('../app');
 
+let elementId;
+describe("API Connectour user test", () => {
+    it("POST /user/signup", (done) => {
+        request(app)
+            .post("/user/signup")
+            .expect("Content-Type", /json/)
+            .send({
+                name: "Leticia",
+                surname: "Silva",
+                userName: "leticia", 
+                county: "Recife", 
+                role: "user", 
+                registrationNumber: 44555, 
+                password: "2323"
+            })
+            .expect(201)
+            .end((err, res) => {
+                if(err) return done(err)
+                elementId = res.body._id
+                return done()
+            })
+    })
+    
+})
