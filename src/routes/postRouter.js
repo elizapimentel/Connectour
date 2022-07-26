690df3bff86fccd7823074d3aae37c167530f518
elizapimentel <elizapimentel@hotmail.com>
2022-07-26 16:55:57 -0300

    :lock: add access permissions

--- src/routes/postRouter.js
+++ src/routes/postRouter.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const router = express.Router();
-
 const controller = require('../controllers/postController')
-// const authController = require('../controllers/login')
-// const { checkAuth } = require('../middleware/auth')
+const { grantAccess } = require('../middleware/auth');
+const { allowIfLoggedin } = require('../middleware/auth')
 
-router.post("/create", controller.creatPost)   
-router.get("/all", controller.getAll)
-router.get("/name", controller.getByName)
-router.put("/update/:id", controller.updatePost)
-router.delete("/delete/:id", controller.deletePost) //gonna need authorization
+router.post("/create", allowIfLoggedin, grantAccess('createOwn', 'post'),controller.creatPost)   
+router.get("/all", allowIfLoggedin, grantAccess('readAny', 'post'), controller.getAll)
+router.get("/name", allowIfLoggedin, grantAccess('readAny', 'post'), controller.getByName)
+router.put("/update/:id", allowIfLoggedin, grantAccess('updateOwn', 'post'), controller.updatePost)
+router.delete("/delete/:id", allowIfLoggedin, grantAccess('deleteAny', 'post'), controller.deletePost) 
 
 module.exports = router
